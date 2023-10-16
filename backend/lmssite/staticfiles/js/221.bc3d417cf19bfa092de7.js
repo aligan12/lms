55f2620e36613dcd18db95563ba8c956ac8be127
Aligan18 <alisherganiev18@gmail.com>
2023-10-16 11:55:12 +0600

    I make backup

--- backend/lmssite/staticfiles/js/221.bc3d417cf19bfa092de7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[221],{221:(n,t,a)=>{a.r(t),a.d(t,{default:()=>s});var e=a(893),r=a(887);const s=function(){var n=(0,r.$G)("about").t;return(0,e.jsx)("div",{children:n("novaya-stranica")})}}}]);
